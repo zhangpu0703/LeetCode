2b4816cbe176f03f0c3d8e6fbdf25e1081f72361
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-05-20 22:21:25 -0400

    [350. Intersection of Two Arrays II][Accepted]committed by LeetCode Extension

---
+++ 350-Intersection-of-Two-Arrays-II/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        List<Integer> list = new ArrayList<>();
+        while (i<nums1.length && j<nums2.length){
+            if (nums1[i]<nums2[j]) i++;
+            else if (nums1[i]>nums2[j]) j++;
+            else {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] res = new int[list.size()];
+        for (int ind=0; ind<res.length; ind++){
+            res[ind]=list.get(ind);
+        }
+        return res;
+    }
+}
