233bd9fed871209a6f5d570d3fa19c004d83f01b
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-05-24 19:44:28 -0400

    [27. Remove Element][Accepted]committed by LeetCode Extension

---
+++ 27-Remove-Element/solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int ind = 0;
+        for (int i=0; i<nums.length; i++){
+            if (nums[i]!=val){
+                nums[ind]=nums[i];
+                ind++;
+            }
+        }
+        return ind;
+    }
+}
