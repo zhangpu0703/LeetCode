dd89d8e4a9d154c93f6958af3fc3bc0c46ccf4dc
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-05-23 22:02:29 -0400

    [24. Swap Nodes in Pairs][Accepted]committed by LeetCode Extension

--- 24-Swap-Nodes-in-Pairs/solution.java
+++ 24-Swap-Nodes-in-Pairs/solution.java
@@ -8,32 +8,17 @@
  */
 public class Solution {
     public ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null) return head;
-        ListNode odd = new ListNode(0);
-        ListNode even = new ListNode(0);
-        ListNode curOdd = odd;
-        ListNode curEven = even;
-        while (head!=null && head.next != null){
-            curOdd.next = head;
-            curEven.next = head.next;
-            head=head.next.next;
-            curOdd=curOdd.next;
-            curEven=curEven.next;
+        ListNode temp = new ListNode (0);
+        temp.next=head;
+        ListNode swap = temp;
+        while (swap.next != null && swap.next.next != null){
+            ListNode n1 = swap.next;
+            ListNode n2 = swap.next.next;
+            n1.next=n2.next;
+            swap.next=n2;
+            swap.next.next=n1;
+            swap=swap.next.next;
         }
-        if (head != null) {
-            curOdd.next = head;
-        }
-        ListNode dummy = new ListNode(0);
-        ListNode cur = dummy;
-        curOdd = odd;
-        curEven = even;
-        while(curOdd!=null && curEven != null){
-            cur.next = curEven.next;
-            cur.next.next = curOdd.next;
-            cur=cur.next.next;
-            curOdd=curOdd.next;
-            curEven=curEven.next;
-        }
-        return dummy.next;
+        return temp.next;
     }
 }
