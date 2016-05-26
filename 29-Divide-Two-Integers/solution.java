16da5cbab6c52fa1a4ac9cd50709dd21a992c8fa
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-05-25 21:39:56 -0400

    [29. Divide Two Integers][Time Limit Exceeded]committed by LeetCode Extension

--- 29-Divide-Two-Integers/solution.java
+++ 29-Divide-Two-Integers/solution.java
@@ -1,11 +1,16 @@
 public class Solution {
     public int divide(int dividend, int divisor) {
         if (divisor==0) return Integer.MAX_VALUE;
+        if (dividend==0) return 0;
+        long ldivisor = divisor;
+        long ldividend = dividend;
         int sign = 1;
-        if ((dividend>0&&divisor<0) || (divisor<0 &&dividend>0)) sign = -1;
-        dividend = Math.abs(dividend);
-        divisor = Math.abs(divisor);
+        if ((ldividend>0 && ldivisor<0) || (ldivisor<0 && ldividend>0)) sign = -1;
+        //if (divisor == Integer.MIN_VALUE) return 0;
+        ldividend = Math.abs(ldividend);
+        ldivisor = Math.abs(ldivisor);
         if (dividend<divisor) return 0;
+        if (dividend==divisor) return 1;
         int res=1;
         int newdiv = divisor;
         while (dividend-newdiv>=newdiv){
