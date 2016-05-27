1874e3a55d63a3c1db6e87facf92f3458c523101
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-05-26 20:09:01 -0400

    [30. Substring with Concatenation of All Words][Accepted]committed by LeetCode Extension

--- 30-Substring-with-Concatenation-of-All-Words/solution.java
+++ 30-Substring-with-Concatenation-of-All-Words/solution.java
@@ -4,18 +4,19 @@ public class Solution {
         List<Integer> res = new ArrayList<Integer>();
         if (n==0) return res;
         int len = words[0].length();
+        HashMap<String, Integer> omap = new HashMap<String, Integer>();
+        for (String ss : words){
+            if (omap.containsKey(ss)) omap.put(ss,omap.get(ss)+1);
+            else omap.put(ss,1);
+        }
         for (int i=0; i<=s.length()-n*len; i++){
             //if ((i+n*len)>s.length()) break;
-            if (helper(s,words,i,len,n)) res.add(i);
+            if (helper(s,omap,i,len,n)) res.add(i);
         }
         return res;
     }
-    public boolean helper(String s, String[] words, int i, int len, int n){
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
-        for (String ss : words){
-            if (map.containsKey(ss)) map.put(ss,map.get(ss)+1);
-            else map.put(ss,1);
-        }
+    public boolean helper(String s, HashMap<String, Integer> omap, int i, int len, int n){
+        HashMap<String, Integer> map = new HashMap<String, Integer>(omap);
         for (int j=0; j<n; j++){
             if (map.isEmpty()) return true;
             String cur = s.substring(i+j*len, i+(j+1)*len);
