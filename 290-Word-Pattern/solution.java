224435aa7bac9c1c058f72ba6855b7094cf43f1d
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-05-29 20:52:26 -0400

    [290. Word Pattern][Accepted]committed by LeetCode Extension

---
+++ 290-Word-Pattern/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        if (pattern.length() != words.length) return false;
+        HashMap <Character, String> map = new HashMap <Character, String>(); 
+        for (int i=0; i<pattern.length(); i++){
+            if (map.containsKey(pattern.charAt(i))){
+                
+                if (!map.get(pattern.charAt(i)).equals(words[i])) return false;
+            }
+            else {
+                if (map.containsValue(words[i])) return false;
+                map.put(pattern.charAt(i), words[i]);
+            }
+        }
+        return true;
+    }
+}
