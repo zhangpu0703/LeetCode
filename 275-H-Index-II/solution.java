c6b6a0946b4bb05964f48fd75579dd922bab6c25
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-06-03 22:18:09 -0400

    [275. H-Index II][Accepted]committed by LeetCode Extension

--- 275-H-Index-II/solution.java
+++ 275-H-Index-II/solution.java
@@ -1,13 +1,15 @@
 public class Solution {
     public int hIndex(int[] citations) {
+        int start=0;
+        int end=citations.length-1;
         if (citations.length==0) return 0;
-        int start = 0;
-        int end = citations.length;
-        while (start<end){
-            int mid = start+(end-start)/2;
-            if (citations[mid]<=mid+1) start = mid;
-            else end = mid-1;
+        if (end == 0) return citations[0]==0? 0: 1;
+        int mid;
+        while (start<=end){
+            mid = start+(end-start)/2;
+            if (citations[mid]>=citations.length-mid) end=mid-1;
+            else start = mid+1;
         }
-        return start;
+        return citations.length-end-1;
     }
 }
