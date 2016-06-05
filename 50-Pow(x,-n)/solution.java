1b70c0c3c5ff7021fc5f13d500bb96362ed6e5d6
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-06-04 20:04:26 -0400

    [50. Pow(x, n)][Accepted]committed by LeetCode Extension

--- 50-Pow(x,-n)/solution.java
+++ 50-Pow(x,-n)/solution.java
@@ -2,7 +2,12 @@ public class Solution {
     public double myPow(double x, int n) {
         if (n==0) return 1;
         if (x==0) return 0;
-        if (n%2 == 0) return myPow(x,n/2) * myPow(x,n/2);
-        else return myPow(x,n/2) * myPow(x,n/2)*x;
+        int half = Math.abs(n/2); 
+        if (n<0){
+            n=-n;
+            x=1/x;
+        }
+        if (n%2 == 0) return myPow(x*x,half);
+        else return myPow(x*x,half)*x;
     }
 }
