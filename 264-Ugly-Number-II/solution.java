3d4fa2f2f0fe040c9ad40328473b83b161012d18
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-06-04 21:57:07 -0400

    [264. Ugly Number II][Accepted]committed by LeetCode Extension

--- 264-Ugly-Number-II/solution.java
+++ 264-Ugly-Number-II/solution.java
@@ -6,15 +6,11 @@ public class Solution {
         int[] res = new int[n];
         res[0]=1;
         for (int i=1; i<n; i++){
-            int curtwo = res[two]*2;
-            int curthree =res[three]*3;
-            int curfive = res[five]*5;
-            int cur = Math.min(curtwo, Math.min(curthree, curfive));
+            int cur = Math.min(res[two]*2, Math.min(res[three]*3, res[five]*5));
+            if (res[two]*2==cur) two++;
+            if (res[three]*3==cur) three++;
+            if (res[five]*5==cur) five++;
             res[i]=cur;
-            if (cur==curtwo) two++;
-            if (cur==curthree) three++;
-            if (cur==curfive) five++;
-            
         }
         return res[n-1];
     }
