0fa1fb2336b85ad77a1d3231bf84db3ae8b96c53
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-06-07 22:28:53 -0400

    [60. Permutation Sequence][Accepted]committed by LeetCode Extension

--- 60-Permutation-Sequence/solution.java
+++ 60-Permutation-Sequence/solution.java
@@ -1,20 +1,21 @@
 public class Solution {
     public String getPermutation(int n, int k) {
-        StringBuilder sb = new StringBuilder();
+        List<Integer> nums = new ArrayList<Integer>();
         int fact = 1;
-        List<Integer> unused = new ArrayList<Integer> ();
-
         for (int i=1; i<=n; i++){
-            fact*=i;
-            unused.add(i);
+            fact *= i;
+            nums.add(i);
         }
+        StringBuilder sb = new StringBuilder();
         k--;
-        for (int j=1; j<=n; j++){
-            fact=fact/(n-j+1);
-            int first = k/fact;
-            sb.append(String.valueOf(unused.get(first)));
-            unused.remove(first);
-            k=k%fact;
+        for(int i=1; i<=n; i++){
+            fact = fact/(n-i+1);
+            
+            int cur = k/fact;
+            k = k%fact;
+            sb.append(nums.get(cur));
+            nums.remove(cur);
+            
         }
         return sb.toString();
     }
