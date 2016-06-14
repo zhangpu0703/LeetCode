aa5a55d22893fb52b7d4257735e0715ac6026d05
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-06-14 12:16:35 -0400

    [328. Odd Even Linked List][Accepted]committed by LeetCode Extension

--- 328-Odd-Even-Linked-List/solution.java
+++ 328-Odd-Even-Linked-List/solution.java
@@ -8,19 +8,18 @@
  */
 public class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if (head == null || head.next == null){
-            return head;
+        if (head == null || head.next == null) return head;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode curEven = even;
+        while (curEven != null && curEven.next!=null){
+            odd.next = curEven.next;
+            curEven.next = odd.next.next;
+            odd = odd.next;
+            curEven=odd.next;
         }
-        ListNode odd=head;
-        ListNode even=head.next;
-        ListNode cureven = even;
-        while (cureven != null && cureven.next != null){
-            odd.next = cureven.next;
-            cureven.next=odd.next.next;
-            odd=odd.next;
-            cureven=cureven.next;
-        }
-        odd.next=even;
+        
+        odd.next = even;
         return head;
     }
 }
