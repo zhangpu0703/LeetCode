7462aee3071d183ccf63448d7b239c4ea665b8fa
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-06-16 19:33:32 -0400

    [86. Partition List][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 86-Partition-List/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode lt = new ListNode(0);
+        ListNode gte = new ListNode(0);
+        ListNode curL = lt;
+        ListNode curG = gte;
+        while(head != null){
+            if (head.val<x){
+                curL.next=head;
+                curL=curL.next;
+            }
+            else{
+                curG.next = head;
+                curG=curG.next;
+            }
+            head=head.next;
+        }
+        curL.next = gte.next;
+        return lt.next;
+    }
+    
+}
