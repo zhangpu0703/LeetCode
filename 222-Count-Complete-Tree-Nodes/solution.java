0dd3b91bc38caa17a263b8e542a7218912d7cb92
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-06-15 21:08:21 -0400

    [222. Count Complete Tree Nodes][Accepted]committed by LeetCode Extension

--- 222-Count-Complete-Tree-Nodes/solution.java
+++ 222-Count-Complete-Tree-Nodes/solution.java
@@ -10,6 +10,14 @@
 public class Solution {
     public int countNodes(TreeNode root) {
         if (root == null) return 0;
-        return 1+countNodes(root.left)+countNodes(root.right);
+        int leftH = height(root.left);
+        int rightH= height(root.right);
+        if (leftH == rightH) return (1<<leftH)+countNodes(root.right);
+        else return (1<<rightH)+countNodes(root.left);
+    }
+    
+    public int height(TreeNode root){
+        if (root == null) return 0;
+        else return 1+height(root.left);
     }
 }
