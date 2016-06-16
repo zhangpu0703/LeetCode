e4f2d257b8c27863361553a86c5e300b34e82f07
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-06-16 15:08:40 -0400

    [35. Search Insert Position][Accepted]committed by LeetCode Extension

--- 35-Search-Insert-Position/solution.java
+++ 35-Search-Insert-Position/solution.java
@@ -2,16 +2,14 @@ public class Solution {
     public int searchInsert(int[] nums, int target) {
         int start = 0;
         int end = nums.length-1;
-        if (target>nums[end]) return end+1;
         if (target<nums[start]) return 0;
-        while (start<end-1){
+        if (target>nums[end]) return nums.length;
+        while (start<end){
             int mid = start+(end-start)/2;
-            if (nums[mid]==target) return mid;
-            if (nums[mid]<target && nums[mid+1]>=target) return mid+1;
-            if (nums[mid]<target) start = mid+1;
+            if (target == nums[mid]) return mid;
+            else if (target>nums[mid]) start = mid+1;
             else end = mid;
         }
-        if (nums[start]>=target) return start;
-        else return start+1;
+        return start;
     }
 }
