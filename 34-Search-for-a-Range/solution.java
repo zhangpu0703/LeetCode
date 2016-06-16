d11625d6caef609510dfec9b189e94e2c248a47c
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-06-16 15:05:13 -0400

    [34. Search for a Range][Time Limit Exceeded]committed by LeetCode Extension

--- 34-Search-for-a-Range/solution.java
+++ 34-Search-for-a-Range/solution.java
@@ -1,36 +1,30 @@
 public class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length-1;
         int[] res = new int[2];
         res[0]=-1;
         res[1]=-1;
-        while (start<end-1){
+        int start = 0;
+        int end = nums.length-1;
+        while (start<end){
             int mid = start+(end-start)/2;
-            if (target>nums[mid]) start = mid;
-            else if (target<nums[mid]) end = mid;
-            else if (mid>0 && nums[mid]==nums[mid-1]) end = mid;
-            else{
-                res[0]=mid;
-                break;
-            }
+            if (nums[mid]<target) start = mid+1;
+            else if (nums[mid]>target) end = mid;
+            else if (mid>0 && nums[mid-1]==nums[mid]) end = mid-1;
+            else break;
         }
         if (nums[start]==target) res[0]=start;
-        else if (start<nums.length-1 && nums[start+1]==target) res[0]=start+1;
-        start = 0;
+        start=0;
         end = nums.length-1;
-        while (start<end-1){
+        while (start<end){
             int mid = start+(end-start)/2;
-            if (target>nums[mid]) start = mid;
-            else if (target<nums[mid]) end = mid;
-            else if (mid<nums.length-1 && nums[mid]==nums[mid+1]) start = mid;
-            else{
-                res[1]=mid;
-                break;
-            }
+            if (nums[mid]<target) start = mid;
+            else if (nums[mid]>target) end = mid-1;
+            else if (mid<nums.length-1 && nums[mid+1]==nums[mid]) start = mid+1;
+            else break;
         }
-        if (nums[end]==target) res[1]=end;
-        else if (end>0 && nums[end-1]==target) res[1]=end-1;
+        if (nums[start]==target) res[1]=start;
+        
         return res;
+        
     }
 }
