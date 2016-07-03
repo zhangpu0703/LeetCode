99289f9e41493d19bd24ba40a64a49fb87b9403e
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-02 21:32:49 -0400

    [133. Clone Graph][Accepted]committed by LeetCode Extension

--- 133-Clone-Graph/solution.java
+++ 133-Clone-Graph/solution.java
@@ -10,12 +10,15 @@ public class Solution {
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
         HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
         Queue<UndirectedGraphNode> queue = new LinkedList<UndirectedGraphNode>();
+        HashSet<UndirectedGraphNode> set = new HashSet<UndirectedGraphNode>();
         if (node == null) return null;
         queue.offer(node);
         while(!queue.isEmpty()){
             int size = queue.size();
             for (int i=0; i<size; i++){
                 UndirectedGraphNode cur = queue.poll();
+                if (set.contains(cur)) continue;
+                set.add(cur);
                 List<UndirectedGraphNode> neibs = cur.neighbors;
                 for (UndirectedGraphNode n : neibs) queue.offer(n);
                 UndirectedGraphNode newNode = new  UndirectedGraphNode(cur.label);
@@ -24,11 +27,14 @@ public class Solution {
              
         }
         
+        set = new HashSet<UndirectedGraphNode>();
         queue.offer(node);
         while(!queue.isEmpty()){
             int size = queue.size();
             for (int i=0; i<size; i++){
                 UndirectedGraphNode old = queue.poll();
+                if (set.contains(old)) continue;
+                set.add(old);
                 List<UndirectedGraphNode> neibs = old.neighbors;
                 List<UndirectedGraphNode> newList = new ArrayList<UndirectedGraphNode>();
                 for (UndirectedGraphNode n : neibs){
