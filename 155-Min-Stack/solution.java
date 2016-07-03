4c2fd38b0f6ddf0976f7d81b1447595c7507cbbd
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-02 22:03:32 -0400

    [155. Min Stack][Accepted]committed by LeetCode Extension

--- 155-Min-Stack/solution.java
+++ 155-Min-Stack/solution.java
@@ -1,28 +1,35 @@
 public class MinStack {
-    Stack<Integer> stack;
-    Stack<Integer> curmin;
+    Stack<Integer> myStack;
+    Stack<Integer> curMin;
+    int min; 
+
     /** initialize your data structure here. */
     public MinStack() {
-        stack= new Stack<Integer>();
-        curmin = new Stack<Integer>();
+        min= Integer.MAX_VALUE;
+        myStack = new Stack<Integer>();
+        curMin = new Stack<Integer>();
     }
     
     public void push(int x) {
-        if (curmin.isEmpty() || x<= curmin.peek()) curmin.push(x);
-        stack.push(x);
+        if (x<=min) min=x;
+        myStack.push(x);
+        curMin.push(min);
     }
     
     public void pop() {
-        if (curmin.peek().equals(stack.peek())) curmin.pop();
-        stack.pop();
+        int a = myStack.pop();
+        
+        curMin.pop();
+        if (curMin.isEmpty()) min = Integer.MAX_VALUE;
+        if (a==min) min=curMin.peek();
     }
     
     public int top() {
-        return stack.peek();
+        return myStack.peek();
     }
     
     public int getMin() {
-        return curmin.peek();
+        return curMin.peek();
     }
 }
 
