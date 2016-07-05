b3237a66ea53e5c3649a492f53ddba02cc76a159
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-05 16:37:13 -0400

    [62. Unique Paths][Accepted]committed by LeetCode Extension

--- 62-Unique-Paths/solution.java
+++ 62-Unique-Paths/solution.java
@@ -1,17 +1,13 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] res = new int[m][n];
-        for (int i=0; i<m; i++){
-            res[i][0]=1;
-        }
-        for (int j = 0; j<n; j++){
-            res[0][j]=1;
-        }
-        for (int i = 1;i<m; i++){
-            for (int j = 1; j<n; j++){
-                res[i][j]=res[i-1][j]+res[i][j-1];
+        int[][] grid = new int[m][n];
+        for (int i=0; i<m; i++) grid[i][0]=1;
+        for (int j=0; j<n; j++) grid[0][j]=1;
+        for (int i=1; i<m; i++){
+            for (int j=1; j<n; j++){
+                grid[i][j]=grid[i-1][j]+grid[i][j-1];
             }
         }
-        return res[m-1][n-1];
+        return grid[m-1][n-1];
     }
 }
