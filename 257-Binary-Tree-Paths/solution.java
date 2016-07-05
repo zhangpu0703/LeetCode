e72593222d127d47b6129887c7597e8aa4eca11f
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-05 12:32:01 -0400

    [257. Binary Tree Paths][Accepted]committed by LeetCode Extension

--- 257-Binary-Tree-Paths/solution.java
+++ 257-Binary-Tree-Paths/solution.java
@@ -8,14 +8,26 @@
  * }
  */
 public class Solution {
+    private List<String> res = new ArrayList<String>();
     public List<String> binaryTreePaths(TreeNode root) {
-        List <String> res = new ArrayList<String>();
-        if (root != null) helper(res,"",root);
+        if (root == null) return res;
+        helper(root,"");
         return res;
     }
-    public void helper(List<String> res, String path, TreeNode root){
-        if (root.left == null && root.right == null) res.add(path+root.val);
-        if (root.left != null) helper(res, path+root.val+"->", root.left);
-        if (root.right != null) helper(res, path+root.val+"->", root.right);
+    public void helper(TreeNode root, String path){
+        //if (root == null) res.add(new ArrayList(path));
+        if (root.left == null && root.right == null){
+            path=path+root.val;
+            res.add(new String(path));
+            return;
+        }
+        if (root.left != null) {
+            String cur =path+root.val+"->";
+            helper(root.left,cur);
+        }
+        if (root.right != null) {
+            String cur =path+root.val+"->";
+            helper(root.right,cur);
+        }
     }
 }
