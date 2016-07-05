8e8b2c6c4c316388314335c6db35b8920da8bc2d
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-05 19:09:05 -0400

    [187. Repeated DNA Sequences][Accepted]committed by LeetCode Extension

--- 187-Repeated-DNA-Sequences/solution.java
+++ 187-Repeated-DNA-Sequences/solution.java
@@ -1,14 +1,11 @@
 public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        HashMap<String, Integer> map = new HashMap<>();
+        List<String> res = new ArrayList<String>();
+        HashSet<String> set = new HashSet<String>();
         for (int i=0; i<=s.length()-10; i++){
             String cur = s.substring(i,i+10);
-            if (map.containsKey(cur)) map.put(cur,map.get(cur)+1);
-            else map.put(cur,1);
-        }
-        List<String> res = new ArrayList<>();
-        for (String ss : map.keySet()){
-            if (map.get(ss)>1) res.add(ss);
+            if (set.contains(cur) && !res.contains(cur)) res.add(cur);
+            else set.add(cur);
         }
         return res;
     }
