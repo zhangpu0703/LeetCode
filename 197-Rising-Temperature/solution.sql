856390f7d1b1b1a267e2cab8fa323e9a29129627
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-05 18:07:53 -0400

    [197. Rising Temperature][Accepted]committed by LeetCode Extension

--- 197-Rising-Temperature/solution.sql
+++ 197-Rising-Temperature/solution.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-Select a.Id from
-Weather a, Weather b
-where TO_DAYS(a.Date)=TO_DAYS(b.Date)+1 and a.Temperature>b.Temperature
+Select a.Id from Weather as a left join Weather as b
+on TO_DAYS(a.Date)=TO_DAYS(b.Date)+1
+where a.Temperature>b.Temperature
