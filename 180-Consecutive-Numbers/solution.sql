c7a9f4e5acebd0d151a1abdfb238aa76c752685e
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-05 18:17:42 -0400

    [180. Consecutive Numbers][Time Limit Exceeded]committed by LeetCode Extension

--- 180-Consecutive-Numbers/solution.sql
+++ 180-Consecutive-Numbers/solution.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select distinct(a.Num) as ConsecutiveNums from Logs as a, Logs b, Logs c
-where (a.Num=b.Num) & (b.Num=c.Num) & (a.Id = b.Id+1) & (b.Id = c.Id+1)
+select distinct(a.Num) as ConsecutiveNums from Logs a, Logs b, Logs c
+where (a.Num=b.Num) & (b.Num=c.Num) & (a.Id = b.Id-1) & (b.Id = c.Id-1)
