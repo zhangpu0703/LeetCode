d0b5a31330d219b4c5846459c779f428d2a05cde
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-05 16:14:04 -0400

    [79. Word Search][Accepted]committed by LeetCode Extension

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -1,56 +1,26 @@
 public class Solution {
     public boolean exist(char[][] board, String word) {
-        if (word.length()==0) return true;
         int m = board.length;
-        if (m==0) return false;
         int n = board[0].length;
-        boolean[][] used = new boolean[m][n];
+        if (word.length()==0) return false;
         for (int i=0; i<m; i++){
             for (int j=0; j<n; j++){
-                if (board[i][j] == word.charAt(0)){
-                    used[i][j]=true;
-                    if (helper(board,word,i,j,1,used)) return true;
-                    used[i][j]=false;
-                }
+                if (helper(board,word,i,j,0)) return true;
             }
         }
         return false;
     }
-    
-    public boolean helper(char[][] board, String word, int i, int j, int ind, boolean[][] used){
+    public boolean helper(char[][] board, String word, int i, int j, int ind){
         if (ind == word.length()) return true;
-        if (i>0){
-            if (!used[i-1][j] && word.charAt(ind)==board[i-1][j]){
-                used[i-1][j]=true;
-                if (helper(board,word,i-1,j,ind+1,used)) return true;
-                used[i-1][j]=false;
-            }
-        }
-        
-        if (i<board.length-1){
-            if (!used[i+1][j] && word.charAt(ind)==board[i+1][j]){
-                used[i+1][j]=true;
-                if (helper(board,word,i+1,j,ind+1,used)) return true;
-                used[i+1][j]=false;
-            }
-        }
-        
-        if (j>0){
-            if (!used[i][j-1] && word.charAt(ind)==board[i][j-1]){
-                used[i][j-1]=true;
-                if (helper(board,word,i,j-1,ind+1,used)) return true;
-                used[i][j-1]=false;
-            }
-        }
-        
-        if (j<board[0].length-1){
-            if (!used[i][j+1] && word.charAt(ind)==board[i][j+1]){
-                used[i][j+1]=true;
-                if (helper(board,word,i,j+1,ind+1,used)) return true;
-                used[i][j+1]=false;
-            }
-        }
-        
+        if (i<0 || i>board.length-1 || j<0 || j>board[0].length-1) return false;
+        char cur = word.charAt(ind);
+        if (board[i][j]!=cur) return false;
+        board[i][j]=' ';
+        if (helper(board,word,i-1,j,ind+1)) return true;
+        if (helper(board,word,i+1,j,ind+1)) return true;
+        if (helper(board,word,i,j+1,ind+1)) return true;
+        if (helper(board,word,i,j-1,ind+1)) return true;
+        board[i][j]=cur;
         return false;
     }
 }
