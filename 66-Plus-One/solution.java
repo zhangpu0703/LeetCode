44923b144ff20f3b8d34b32ac15439ecf93af0b6
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-05 15:35:08 -0400

    [66. Plus One][Accepted]committed by LeetCode Extension

--- 66-Plus-One/solution.java
+++ 66-Plus-One/solution.java
@@ -1,27 +1,21 @@
 public class Solution {
     public int[] plusOne(int[] digits) {
-        int last=digits.length;
-        if (digits[last-1]!=9){
-            digits[last-1]++;
-            return digits;
-        }
-        else{
-            while (last>0 && digits[last-1]==9){
-                digits[last-1]=0;
-                last--;
-            }
-            if (last!=0){
-                digits[last-1]+=1;
+        int end = digits.length-1;
+        while (end>=0){
+            if (digits[end]<9){
+                digits[end]++;
                 return digits;
             }
             else{
-                int[] aa = new int [digits.length+1];
-                aa[0]=1;
-                for (int i=0;i<digits.length;i++){
-                    aa[i+1]=digits[i];
-                }
-                return aa;
+                digits[end]=0;
+                end--;
             }
         }
+        if (digits[0] == 0){
+            int[] res = new int[digits.length+1];
+            res[0]=1;
+            return res;
+        }
+        return digits;
     }
 }
