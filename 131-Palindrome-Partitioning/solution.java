169a317760fb03a955eebc3ebb0c85b1cd976fb6
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-12 19:12:14 -0400

    [131. Palindrome Partitioning][Accepted]committed by LeetCode Extension

--- 131-Palindrome-Partitioning/solution.java
+++ 131-Palindrome-Partitioning/solution.java
@@ -1,22 +1,28 @@
 public class Solution {
-    List<List<String>> res = new ArrayList<List<String>>();
+    private List<List<String>> res = new ArrayList<List<String>>();
     public List<List<String>> partition(String s) {
         helper(s,0, new ArrayList<String>());
         return res;
     }
-    public void helper(String s, int start, List<String> path){
-        if (start==s.length()) res.add(new ArrayList(path));
-        for (int i= start; i<s.length(); i++){
-            if (isPalin(s,start,i)){
-                path.add(s.substring(start,i+1));
-                helper(s,i+1,path);
+    
+    public void helper(String s, int curInd, List<String> path){
+        if (curInd == s.length()){
+            res.add(new ArrayList(path));
+            return;
+        }
+        for (int i=curInd+1; i<=s.length(); i++){
+            if (isPal(s.substring(curInd,i))){
+                path.add(s.substring(curInd,i));
+                helper(s,i,path);
                 path.remove(path.size()-1);
             }
         }
     }
-    public boolean isPalin(String s, int start, int end){
+    
+    public boolean isPal(String s){
+        int start = 0, end = s.length()-1;
         while (start<end){
-            if (s.charAt(start) != s.charAt(end)) return false;
+            if (s.charAt(start)!=s.charAt(end)) return false;
             start++;
             end--;
         }
