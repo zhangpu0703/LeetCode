da449eaf6ec8350ce9afe6159f10800f71fdf52e
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-12 18:27:05 -0400

    [54. Spiral Matrix][Accepted]committed by LeetCode Extension

--- 54-Spiral-Matrix/solution.java
+++ 54-Spiral-Matrix/solution.java
@@ -4,32 +4,28 @@ public class Solution {
         int m = matrix.length;
         if (m==0) return res;
         int n = matrix[0].length;
-        int left = 0;
-        int right = n-1;
-        int top = 0;
-        int bottom = m-1;
-        while (left<=right && top<=bottom){
-            for (int j = left; j<=right; j++){
-                res.add(matrix[top][j]);
+        int left = 0, right = n-1, top = 0, bottem = m-1;
+        while (left<=right && top<=bottem){
+            for (int i=left; i<=right; i++){
+                res.add(matrix[top][i]);
             }
             top++;
-            for (int i=top; i<=bottom; i++){
+            for (int i=top; i<=bottem; i++){
                 res.add(matrix[i][right]);
             }
             right--;
-            if (top<=bottom){
-                for (int j = right; j>=left; j--){
-                    res.add(matrix[bottom][j]);
+            if (top<=bottem){
+                for (int i=right; i>=left; i--){
+                    res.add(matrix[bottem][i]);
                 }
-                bottom--;
+                bottem--;
             }
             if (left<=right){
-                for (int i=bottom; i>=top; i--){
+                for (int i=bottem; i>=top; i--){
                     res.add(matrix[i][left]);
                 }
                 left++;
             }
-
         }
         return res;
     }
