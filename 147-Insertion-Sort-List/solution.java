a28c8cd1901a0f0a5ecbf52b72bf21d71e0c7ad8
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-18 11:36:39 -0400

    [147. Insertion Sort List][Accepted]committed by LeetCode Extension

--- 147-Insertion-Sort-List/solution.java
+++ 147-Insertion-Sort-List/solution.java
@@ -8,17 +8,18 @@
  */
 public class Solution {
     public ListNode insertionSortList(ListNode head) {
-        ListNode res=new ListNode(0);
-        ListNode cur = head;
         if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(0);
+        ListNode cur = head;
+        
         while (cur != null){
             ListNode temp = cur.next;
-            ListNode begin = res;
-            while (begin.next != null && begin.next.val<cur.val) begin = begin.next;
+            ListNode begin = dummy;
+            while (begin.next!=null && begin.next.val<cur.val) begin=begin.next;
             cur.next = begin.next;
             begin.next = cur;
             cur = temp;
         }
-        return res.next;
+        return dummy.next;
     }
 }
