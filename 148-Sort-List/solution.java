c21058851c3c9b2f419d53e6813997ad5da2a14c
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-20 14:29:37 -0400

    [148. Sort List][Accepted]committed by LeetCode Extension

--- 148-Sort-List/solution.java
+++ 148-Sort-List/solution.java
@@ -8,41 +8,39 @@
  */
 public class Solution {
     public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) return head;
-        ListNode fast = head;
+        if (head==null || head.next == null) return head;
         ListNode slow = head;
-        ListNode prev = null;
-        while(fast != null && fast.next != null){
-            prev = slow;
+        ListNode fast = head;
+        ListNode first = null;
+        while (fast!=null && fast.next!=null){
             fast = fast.next.next;
-            slow = slow.next;
+            first = slow;
+            slow=slow.next;
         }
-        prev.next = null;
-        
-        return merge(sortList(head),sortList(slow));
+        first.next = null;
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+        return merge(l1,l2);
     }
-    
     public ListNode merge(ListNode l1, ListNode l2){
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
         ListNode dummy = new ListNode(0);
         ListNode cur = dummy;
         while (l1 != null || l2 != null){
             if (l1 == null){
-                cur.next = l2;
+                cur.next=new ListNode(l2.val);
                 l2 = l2.next;
             }
             else if (l2 == null){
-                cur.next = l1;
+                cur.next=new ListNode(l1.val);
                 l1 = l1.next;
             }
             else{
-                if (l2.val>l1.val){
-                    cur.next = l1;
+                if (l1.val<l2.val){
+                    cur.next=new ListNode(l1.val);
                     l1 = l1.next;
                 }
                 else{
-                    cur.next = l2;
+                    cur.next=new ListNode(l2.val);
                     l2 = l2.next;
                 }
             }
