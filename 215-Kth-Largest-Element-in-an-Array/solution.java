14d1513412f9ceade3088a92b18148fda6c9cd77
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-25 12:14:11 -0400

    [215. Kth Largest Element in an Array][Time Limit Exceeded]committed by LeetCode Extension

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -1,34 +1,29 @@
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int start = 0;
-        int end = nums.length-1;
-        k = nums.length - k;
+        k = nums.length-k;
+        int start = 0, end = nums.length-1;
         while (start<end){
-            int pivot = quickselect(nums,start,end);
-            if (pivot == k) return nums[pivot];
-            else if (pivot < k) start=pivot+1;
-            else end = pivot-1;
-            
+            int ind = partition(nums,0,nums.length-1);
+            if (ind == k) return nums[ind];
+            else if (ind<k) start = ind+1;
+            else end = ind-1;
         }
         return nums[start];
     }
-    public int quickselect(int[] nums, int start, int end){
+    public int partition(int[] nums, int start, int end){
         int pivot = end;
-        //int smaller = start;
-        int i = start; 
-        int j = end;
+        int i = start, j = end;
         while (i<j){
-            while (i<j && nums[i]<=nums[pivot]) i++;
-            while (i<j && nums[j]>=nums[pivot]) j--;
-            if (i<j){
-                int temp = nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-            }
+            while (i<j && nums[i]<nums[pivot]) i++;
+            while (i<j && nums[j]>nums[pivot]) j--;
+            swap(nums,i,j);
         }
-        int cur = nums[pivot];
-        nums[pivot]=nums[j];
-        nums[j]=cur;
+        swap(nums,j,pivot);
         return j;
+    }
+    public void swap(int[] nums, int a, int b){
+        int temp = nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
     }
 }
