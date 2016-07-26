a6a39a950e3d78d97d873d77a83fa0ff254aeac7
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-26 17:07:37 -0400

    [118. Pascal's Triangle][Accepted]committed by LeetCode Extension

--- 118-Pascal's-Triangle/solution.java
+++ 118-Pascal's-Triangle/solution.java
@@ -1,25 +1,21 @@
 public class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
-        List<Integer> path = new ArrayList<>();
-        if (numRows==0) return res;
-        path.add(1);
-        helper(res,numRows,0,path);
+        if (numRows == 0) return res;
+        List<Integer> first = new ArrayList<>();
+        first.add(1);
+        res.add(first);
+        for (int i=2; i<=numRows; i++){
+            List<Integer> last = res.get(res.size()-1);
+            List<Integer> cur = new ArrayList<>();
+            cur.add(1);
+            for (int j=0; j<last.size()-1; j++){
+                cur.add(last.get(j)+last.get(j+1));
+            }
+            cur.add(1);
+            res.add(cur);
+        }
         return res;
     }
-    public void helper(List<List<Integer>> res, int numRows, int start, List<Integer> path){
-        if (start>=numRows) return;
-        res.add(path);
-        List<Integer> copy = new ArrayList<>();
-        copy.add(1);
-        for (int i=0; i<path.size(); i++){
-            if (i==path.size()-1) copy.add(1);
-            else{
-                copy.add(path.get(i)+path.get(i+1));
-            }
-        }
-        path = new ArrayList<>(copy);
-        
-        helper(res,numRows,start+1,path);
-    }
+    
 }
