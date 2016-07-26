db73dae026b85a455cd6b2e1a0d0877cc7f6544e
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-26 18:14:45 -0400

    [33. Search in Rotated Sorted Array][Time Limit Exceeded]committed by LeetCode Extension

--- 33-Search-in-Rotated-Sorted-Array/solution.java
+++ 33-Search-in-Rotated-Sorted-Array/solution.java
@@ -1,8 +1,7 @@
 public class Solution {
     public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length-1;
-        while (start<end-1){
+        int start = 0, end = nums.length-1;
+        while (start<end){
             int mid = start+(end-start)/2;
             if (nums[mid]==target) return mid;
             if (nums[mid]>nums[end]){
@@ -10,13 +9,12 @@ public class Solution {
                 else end = mid;
             }
             else{
-                if (target>nums[end] || target<nums[mid]) end = mid;
-                else start = mid;
+                if (target<nums[start] && target>nums[mid]) start=mid;
+                else end = mid;
             }
-            
         }
-        if (nums[start]==target) return start;
+        if (nums[start] == target) return start;
         else if (start<nums.length-1 && nums[start+1]==target) return start+1;
-        else return -1;
+        return -1;
     }
 }
