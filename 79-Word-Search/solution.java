fea7f26a913fe6e257aa1ebfb6a9e699aa69cf54
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-26 16:53:31 -0400

    [79. Word Search][Accepted]committed by LeetCode Extension

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -2,25 +2,28 @@ public class Solution {
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
-        if (word.length()==0) return false;
+        boolean[][] used = new boolean[m][n];
         for (int i=0; i<m; i++){
             for (int j=0; j<n; j++){
-                if (helper(board,word,i,j,0)) return true;
+                if (search(board,used,word,i,j,0)) return true;
             }
         }
         return false;
     }
-    public boolean helper(char[][] board, String word, int i, int j, int ind){
-        if (ind == word.length()) return true;
-        if (i<0 || i>board.length-1 || j<0 || j>board[0].length-1) return false;
-        char cur = word.charAt(ind);
-        if (board[i][j]!=cur) return false;
-        board[i][j]=' ';
-        if (helper(board,word,i-1,j,ind+1)) return true;
-        if (helper(board,word,i+1,j,ind+1)) return true;
-        if (helper(board,word,i,j+1,ind+1)) return true;
-        if (helper(board,word,i,j-1,ind+1)) return true;
-        board[i][j]=cur;
-        return false;
+    public boolean search(char[][] board, boolean[][] used, String word, int i, int j, int cur){
+        
+        if (cur == word.length()) return true;
+        if (i<0 || i>=board.length || j<0 || j>=board[0].length) return false;
+        if (word.charAt(cur)!=board[i][j]) return false;
+        if (!used[i][j]){
+            used[i][j]=true;
+            if (search(board,used,word,i-1,j,cur+1)) return true;
+            if (search(board,used,word,i+1,j,cur+1)) return true;
+            if (search(board,used,word,i,j-1,cur+1)) return true;
+            if (search(board,used,word,i,j+1,cur+1)) return true;
+            used[i][j]=false;
+            return false;
+        }
+        else return false;
     }
 }
