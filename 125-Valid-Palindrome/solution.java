135a4bdf15ae510490f95d73e713eef88a8bde1c
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-27 12:36:32 -0400

    [125. Valid Palindrome][Accepted]committed by LeetCode Extension

--- 125-Valid-Palindrome/solution.java
+++ 125-Valid-Palindrome/solution.java
@@ -1,21 +1,12 @@
 public class Solution {
     public boolean isPalindrome(String s) {
-        /*
-        s=s.toLowerCase();
         StringBuilder sb = new StringBuilder();
-        for (int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-            if ((c>='a' && c <='z') || (c>='0' && c <='9')){
-                sb.append(c);
-            }
+        s=s.toLowerCase();
+        for (char c : s.toCharArray()){
+            if ('0'<=c && c<='9') sb.append(c);
+            else if ('a'<=c && c<='z') sb.append(c);
         }
-        String first = sb.toString();
-        return sb.reverse().toString().equals(first);
-        */
-        s=s.toLowerCase().replaceAll("[^a-z0-9]","");
-        for (int i=0; i<s.length()/2; i++){
-            if (s.charAt(i) != s.charAt(s.length()-i-1)) return false;
-        }
-        return true;
+        String a = sb.toString(), b= sb.reverse().toString(); 
+        return a.equals(b);
     }
 }
