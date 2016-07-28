4229ec341dd4920758d2728038e1b5abbae8cafd
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-28 11:53:27 -0400

    [5. Longest Palindromic Substring][Time Limit Exceeded]committed by LeetCode Extension

--- 5-Longest-Palindromic-Substring/solution.java
+++ 5-Longest-Palindromic-Substring/solution.java
@@ -1,20 +1,18 @@
 public class Solution {
     public String longestPalindrome(String s) {
-        int len = 0;
+        if (s.length()==0) return "";
         String res = "";
-        int left = 0;
-        int right = 0;
         for (int i=0; i<s.length(); i++){
-            left = i;
-            right = i;
-            while (right<s.length()-1 && s.charAt(right+1) == s.charAt(right)) right++;
-            while (left>0 && right<s.length()-1 && s.charAt(left-1)==s.charAt(right+1)){
-                left--;
-                right++;
+            int left = i, right = i;
+            while (right<s.length()-1 && s.charAt(right)==s.charAt(right+1)) right++;
+            while (left>0 && right<s.length()-1){
+                if (s.charAt(left-1)==s.charAt(right+1)){
+                    left--;
+                    right++;
+                }
             }
-            if (len < right - left+1){
+            if (res.length()<right-left+1){
                 res = s.substring(left,right+1);
-                len = right - left +1 ;
             }
         }
         return res;
