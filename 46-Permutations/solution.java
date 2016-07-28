b28db75811c49bd4f8344fc5a3fa0e8faacd9461
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-28 11:40:32 -0400

    [46. Permutations][Accepted]committed by LeetCode Extension

--- 46-Permutations/solution.java
+++ 46-Permutations/solution.java
@@ -2,10 +2,10 @@ public class Solution {
     private List<List<Integer>> res = new ArrayList<List<Integer>>();
     public List<List<Integer>> permute(int[] nums) {
         boolean[] used = new boolean[nums.length]; 
-        helper(nums, new ArrayList<Integer>(),0, used);
+        helper(nums, new ArrayList<Integer>(), used);
         return res;
     }
-    public void helper(int[] nums, List<Integer> path, int start, boolean[] used){
+    public void helper(int[] nums, List<Integer> path, boolean[] used){
         if (path.size()==nums.length){
             res.add(new ArrayList(path));
             return;
@@ -14,7 +14,7 @@ public class Solution {
             if (used[i]) continue;
             path.add(nums[i]);
             used[i]=true;
-            helper(nums,path,start+1,used);
+            helper(nums,path,used);
             used[i]=false;
             path.remove(path.size()-1);
         }
