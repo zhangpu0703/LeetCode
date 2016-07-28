efdd69b9c9707dc063ebad4b09f632429d57ad2f
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-28 11:38:49 -0400

    [47. Permutations II][Accepted]committed by LeetCode Extension

--- 47-Permutations-II/solution.java
+++ 47-Permutations-II/solution.java
@@ -1,26 +1,25 @@
 public class Solution {
+    private List<List<Integer>> res = new ArrayList<List<Integer>>();
+    private boolean[] used;
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        if (nums.length==0) return res;
         Arrays.sort(nums);
-        boolean[] used = new boolean[nums.length];
-        helper(res,new ArrayList<Integer>(), nums, used);
+        used=new boolean[nums.length];
+        helper(nums, new ArrayList<Integer>(), 0);
         return res;
     }
-    public void helper(List<List<Integer>> res, List<Integer> path, int[] nums, boolean[] used){
+    public void helper(int[] nums, List<Integer> path, int start){
         if (path.size()==nums.length){
-            res.add(new ArrayList<Integer>(path));
+            res.add(new ArrayList(path));
             return;
         }
-        for (int i = 0; i<nums.length; i++){
-            if (i>0 && nums[i]==nums[i-1] && !used[i-1]) continue;
-            if (!used[i]){
-                path.add(nums[i]);
-                used[i]=true;
-                helper(res,path,nums,used);
-                path.remove(path.size()-1);
-                used[i]=false;
-            } 
+        for (int i=0; i<nums.length; i++){
+            if (used[i]) continue;
+            if (i>0 && nums[i] == nums[i-1] && !used[i-1]) continue;
+            path.add(nums[i]);
+            used[i]=true;
+            helper(nums, path,start+1);
+            path.remove(path.size()-1);
+            used[i]=false;
         }
     }
 }
