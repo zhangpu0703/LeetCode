417aee72a52d90e3c654dd1c4b6a38eabae8d94c
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-28 11:20:33 -0400

    [113. Path Sum II][Accepted]committed by LeetCode Extension

--- 113-Path-Sum-II/solution.java
+++ 113-Path-Sum-II/solution.java
@@ -8,22 +8,20 @@
  * }
  */
 public class Solution {
+    private List<List<Integer>> res = new ArrayList<List<Integer>>();
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        helper(res,root,sum,new ArrayList<Integer>());
+        helper(root, sum, new ArrayList<Integer>());
         return res;
     }
-    public void helper(List<List<Integer>> res, TreeNode root, int sum, List<Integer> path){
+    public void helper(TreeNode root, int sum,  List<Integer> path){
         if (root == null) return;
         path.add(root.val);
-        if (root.val == sum && root.left == null && root.right == null){
-            //path.add(root.val);
-            res.add(new ArrayList<Integer>(path));
+        if (root.left == null && root.right == null && root.val == sum){
+            res.add(new ArrayList(path));
             return;
         }
-        else{
-            helper(res, root.left, sum-root.val,new ArrayList<>(path));
-            helper(res, root.right, sum-root.val,new ArrayList<>(path));
-        }
+        helper(root.left, sum-root.val, new ArrayList(path));
+        helper(root.right, sum-root.val, new ArrayList(path));
+        
     }
 }
