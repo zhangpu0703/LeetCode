503058aba629544fe82ecc01b6b57d27935daa71
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-07-29 14:22:35 -0400

    [93. Restore IP Addresses][Accepted]committed by LeetCode Extension

--- 93-Restore-IP-Addresses/solution.java
+++ 93-Restore-IP-Addresses/solution.java
@@ -1,24 +1,25 @@
 public class Solution {
-    private List<String> res = new ArrayList<String>();
+    List<String> res = new ArrayList<>();
     public List<String> restoreIpAddresses(String s) {
-        helper(s,"",4,0);
+        helper(s,4,0,"");
         return res;
     }
-    public void helper(String s, String path, int remain, int ind){
-        if (remain==0 && ind == s.length()){
+    public void helper(String s, int n, int start, String path){
+        if (n==0 && start == s.length()){
             res.add(new String(path.substring(0,path.length()-1)));
             return;
         }
-        else if (remain == 0 || ind == s.length()) return;
-        String one = s.substring(ind,ind+1);
-        helper(s,path+one+".",remain-1, ind+1);
-        if (ind+2<=s.length() && s.charAt(ind)!='0'){
-            String two = s.substring(ind,ind+2);
-            helper(s,path+two+".",remain-1, ind+2);
+        if (n==0 || start == s.length()) return;
+        String one, two, three;
+        one = s.substring(start,start+1);
+        helper(s,n-1,start+1,path+one+".");
+        if (start+2<=s.length() && s.charAt(start)!='0') {
+            two = s.substring(start,start+2);
+            helper(s,n-1,start+2,path+two+".");
         }
-        if (ind+3<=s.length() && s.charAt(ind)!='0'){
-            String three = s.substring(ind,ind+3);
-            if (Integer.parseInt(three)<=255) helper(s,path+three+".",remain-1, ind+3);
+        if (start+3<=s.length() && s.charAt(start)!='0') {
+            three = s.substring(start,start+3);
+            if (Integer.parseInt(three)<=255) helper(s,n-1,start+3,path+three+".");
         }
     }
 }
