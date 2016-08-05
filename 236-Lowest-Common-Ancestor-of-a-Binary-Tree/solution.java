dd84a96bb4ab218367fe3c90cadf822275b3fdec
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-08-05 11:19:37 -0400

    [236. Lowest Common Ancestor of a Binary Tree][Accepted]committed by LeetCode Extension

--- 236-Lowest-Common-Ancestor-of-a-Binary-Tree/solution.java
+++ 236-Lowest-Common-Ancestor-of-a-Binary-Tree/solution.java
@@ -9,14 +9,11 @@
  */
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return null;
-        if (root == p || root == q) return root;
-        TreeNode left = lowestCommonAncestor(root.left, p , q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if (left!=null && right != null) return root;
-        else if (left == null || right != null) return right;
-        else if (left != null || right == null) return left;
-        else return null;
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if (left != null && right != null) return root;
+        else if (left == null) return right;
+        else return left;
     }
-    
 }
