8f8715ea5263cbddbc9674d960df0224bb254248
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-08-05 12:37:34 -0400

    [347. Top K Frequent Elements][Accepted]committed by LeetCode Extension

--- 347-Top-K-Frequent-Elements/solution.java
+++ 347-Top-K-Frequent-Elements/solution.java
@@ -1,33 +1,37 @@
-class pair{
-    int value;
-    int freq;
-    public pair (int value, int freq){
-        this.value=value;
-        this.freq = freq;
-    }
-}
 public class Solution {
-    Comparator<pair> pairComp = new Comparator<pair>(){
-        @Override
-        public int compare(pair p1, pair p2){
-            return p2.freq-p1.freq;
+    class node{
+        int num, freq;
+        public node(int num, int freq){
+            this.num=num;
+            this.freq = freq;
+        }
+    }
+    private Comparator<node> nodeComp = new Comparator<node>(){
+        public int compare(node a, node b){
+            return a.freq-b.freq;
         }
     };
-    
     public List<Integer> topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-        for (int num:nums){
-            if (map.containsKey(num)) map.put(num,map.get(num)+1);
-            else map.put(num,1);
+        PriorityQueue<node> queue = new PriorityQueue<node>(k,nodeComp);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i<nums.length; i++){
+            if (map.containsKey(nums[i])) map.put(nums[i],map.get(nums[i])+1);
+            else map.put(nums[i],1);
         }
-        PriorityQueue<pair> queue = new PriorityQueue<pair>(pairComp);
-        for (int num : map.keySet()){
-            pair cur = new pair(num,map.get(num));
-            queue.offer(cur);
+        for (int key : map.keySet()){
+            node cur = new node(key,map.get(key));
+            if (queue.size()<k) queue.offer(cur);
+            else{
+                node temp = queue.peek();
+                if (temp.freq<=cur.freq){
+                    queue.poll();
+                    queue.offer(cur);
+                }
+            }
         }
         List<Integer> res = new ArrayList<Integer>();
-        for (int i=0; i<k; i++){
-            res.add(queue.poll().value);
+        while (!queue.isEmpty()){
+            res.add(queue.poll().num);
         }
         return res;
     }
