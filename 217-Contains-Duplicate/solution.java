ed70d26e210a816b9fd944bf819883fc6383fc48
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-08-05 11:38:10 -0400

    [217. Contains Duplicate][Accepted]committed by LeetCode Extension

--- 217-Contains-Duplicate/solution.java
+++ 217-Contains-Duplicate/solution.java
@@ -1,9 +1,9 @@
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<Integer>();
+        HashSet<Integer> set = new HashSet<>();
         for (int num : nums){
             if (set.contains(num)) return true;
-            else set.add(num);
+            set.add(num);
         }
         return false;
     }
