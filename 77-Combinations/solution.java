e92204cb515759caf3a09a9ae4472604e92c5db9
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-08-10 13:27:26 -0400

    [77. Combinations][Accepted]committed by LeetCode Extension

--- 77-Combinations/solution.java
+++ 77-Combinations/solution.java
@@ -1,24 +1,18 @@
 public class Solution {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
     public List<List<Integer>> combine(int n, int k) {
-        boolean[] used = new boolean[n];
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        helper(res, new ArrayList<Integer>(), n, k, used, 1);
+        helper(n,k,1,new ArrayList<Integer>());
         return res;
     }
-    public void helper(List<List<Integer>> res, List<Integer> path, int n, int k, boolean[] used, int start){
+    public void helper(int n, int k, int start, List<Integer> path){
         if (k==0){
-            res.add(new ArrayList<Integer>(path));
+            res.add(new ArrayList(path));
             return;
         }
-        for (int i = start; i<=n; i++){
-            if (used[i-1]) continue;
-            else{
-                path.add(i);
-                used[i-1]=true;
-                helper(res,path,n,k-1,used,i);
-                used[i-1]=false;
-                path.remove(path.size()-1);
-            }
+        for (int i=start; i<=n; i++){
+            path.add(i);
+            helper(n,k-1,i+1,path);
+            path.remove(path.size()-1);
         }
     }
 }
