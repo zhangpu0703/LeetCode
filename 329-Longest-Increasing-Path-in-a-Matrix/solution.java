98cd073bbdaf2abb3f353fe846d4628517512da3
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-08-15 14:36:24 -0400

    [329. Longest Increasing Path in a Matrix][Accepted]committed by LeetCode Extension

--- 329-Longest-Increasing-Path-in-a-Matrix/solution.java
+++ 329-Longest-Increasing-Path-in-a-Matrix/solution.java
@@ -1,27 +1,23 @@
 public class Solution {
     public int longestIncreasingPath(int[][] matrix) {
-        int m = matrix.length;
-        if (m==0) return 0;
-        int n = matrix[0].length;
-        int[][] count = new int[m][n];
+        if (matrix.length == 0 || matrix[0].length == 0) return 0;
         int res = 1;
-        for (int i=0; i<m; i++){
-            for (int j=0; j<n; j++){
-                int cur = helper(matrix,i,j,count);
-                res=Math.max(res,cur);
+        int[][] length = new int[matrix.length][matrix[0].length];
+        for (int i=0; i<matrix.length; i++){
+            for (int j=0; j<matrix[i].length; j++){
+                res = Math.max(res,genLen(i,j,matrix,length));
             }
         }
         return res;
     }
-    public int helper(int[][] matrix, int i, int j, int[][] count){
+    public int genLen (int i, int j, int[][] matrix, int[][] count){
+        //if (i<0 || i>=matrix.length || j<0 || j>=matrix[i].length) return 0;
         if (count[i][j]!=0) return count[i][j];
-        int m = matrix.length;
-        int n = matrix[0].length;
         int len = 1;
-        if (i+1<m && matrix[i+1][j]>matrix[i][j]) len=Math.max(len,1+helper(matrix,i+1,j,count));
-        if (i-1>=0 && matrix[i-1][j]>matrix[i][j]) len=Math.max(len,1+helper(matrix,i-1,j,count));
-        if (j-1>=0 && matrix[i][j-1]>matrix[i][j]) len=Math.max(len,1+helper(matrix,i,j-1,count));
-        if (j+1<n && matrix[i][j+1]>matrix[i][j]) len=Math.max(len,1+helper(matrix,i,j+1,count));
+        if (i<matrix.length-1 && matrix[i+1][j]>matrix[i][j]) len = Math.max(len,1+genLen(i+1,j,matrix,count));
+        if (i>0 && matrix[i-1][j]>matrix[i][j]) len = Math.max(len,1+genLen(i-1,j,matrix,count));
+        if (j>0 && matrix[i][j-1]>matrix[i][j]) len = Math.max(len,1+genLen(i,j-1,matrix,count));
+        if (j<matrix[i].length-1 && matrix[i][j+1]>matrix[i][j]) len = Math.max(len,1+genLen(i,j+1,matrix,count));
         count[i][j]=len;
         return len;
     }
