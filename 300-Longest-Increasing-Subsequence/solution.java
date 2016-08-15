dae927a686347fb915e86c88ccfa027c6352b383
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-08-15 14:18:52 -0400

    [300. Longest Increasing Subsequence][Accepted]committed by LeetCode Extension

--- 300-Longest-Increasing-Subsequence/solution.java
+++ 300-Longest-Increasing-Subsequence/solution.java
@@ -1,14 +1,16 @@
 public class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] res = new int[nums.length];
-        Arrays.fill(res,1);
+        if (nums==null || nums.length==0) return 0; 
+        int[] dp = new int[nums.length];
+        dp[0]=1;
         for (int i=1; i<nums.length; i++){
+            dp[i]=1;
             for (int j=0; j<i; j++){
-                if (nums[j]<nums[i]) res[i]=Math.max(res[i],res[j]+1);
+                if (nums[j]<nums[i]) dp[i]=Math.max(dp[i],dp[j]+1);
             }
         }
-        int max = 0;
-        for (int r : res) max = Math.max(r,max);
-        return max;
+        int res = 1;
+        for (int len : dp) res = Math.max(res,len);
+        return res;
     }
 }
