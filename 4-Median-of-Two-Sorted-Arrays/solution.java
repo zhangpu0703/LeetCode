3b1100ff9694af770a9b506c6ebef70849950fea
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-08-15 13:07:50 -0400

    [4. Median of Two Sorted Arrays][Accepted]committed by LeetCode Extension

--- 4-Median-of-Two-Sorted-Arrays/solution.java
+++ 4-Median-of-Two-Sorted-Arrays/solution.java
@@ -4,20 +4,16 @@ public class Solution {
         int n = nums2.length;
         int l = (m + n + 1) / 2;
         int r = (m + n + 2) / 2;
-        return (getkth(nums1, 0, nums2, 0, l) + getkth(nums1, 0, nums2, 0, r)) / 2.0;
+        return (kthSamllest(nums1, 0, nums2, 0, l) + kthSamllest(nums1, 0, nums2, 0, r)) / 2.0;
     }
-    public double getkth(int[] A, int aStart, int[] B, int bStart, int k){
-        if (aStart>=A.length) return B[k+bStart-1];
-        if (bStart>=B.length) return A[k+aStart-1];
+    public double kthSamllest(int[] A, int aStart, int[] B, int bStart, int k){
+        if (aStart>=A.length) return B[bStart+k-1];
+        if (bStart>=B.length) return A[aStart+k-1];
         if (k == 1) return Math.min(A[aStart], B[bStart]);
-        int aMid = Integer.MAX_VALUE, bMid = Integer.MAX_VALUE;
-        if (aStart + k/2 - 1 < A.length) aMid = A[aStart + k/2 - 1]; 
-        if (bStart + k/2 - 1 < B.length) bMid = B[bStart + k/2 - 1];        
-    
-        if (aMid < bMid) 
-            return getkth(A, aStart + k/2, B, bStart, k - k/2);
-        else 
-            return getkth(A, aStart, B, bStart + k/2, k - k/2);
+        int aMid = aStart+k/2-1 >= A.length? Integer.MAX_VALUE : A[aStart+k/2-1]; 
+        int bMid = bStart+k/2-1 >= B.length? Integer.MAX_VALUE : B[bStart+k/2-1];
+        if (aMid>bMid) return kthSamllest(A,aStart,B,bStart+k/2,k-k/2);
+        else return kthSamllest(A,aStart+k/2,B,bStart,k-k/2);
     }
     
 }
