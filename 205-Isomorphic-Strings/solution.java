c6eb06cf80d1e94751a1c32880cd29ec3aa86cb0
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-08-17 11:02:34 -0400

    [205. Isomorphic Strings][Accepted]committed by LeetCode Extension

--- 205-Isomorphic-Strings/solution.java
+++ 205-Isomorphic-Strings/solution.java
@@ -2,13 +2,15 @@ public class Solution {
     public boolean isIsomorphic(String s, String t) {
         if (s.length()!=t.length()) return false;
         HashMap<Character,Character> map = new HashMap<Character,Character>();
-        for(int i=0; i<s.length(); i++){
-            if (!map.containsKey(s.charAt(i))){
-                if (map.containsValue(t.charAt(i))) return false;
-                map.put(s.charAt(i),t.charAt(i));
-            }
-            else{
+        HashSet<Character> set = new HashSet<>();
+        for (int i=0; i<s.length(); i++){
+            if (map.containsKey(s.charAt(i))){
                 if (map.get(s.charAt(i))!=t.charAt(i)) return false;
+            }
+            else {
+                if (set.contains(t.charAt(i))) return false;
+                set.add(t.charAt(i));
+                map.put(s.charAt(i),t.charAt(i));
             }
         }
         return true;
