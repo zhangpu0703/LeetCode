baeb3d6b5c53b2a93608d03b2b52f28f5fba4974
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-08-18 13:34:58 -0400

    [100. Same Tree][Accepted]committed by LeetCode Extension

--- 100-Same-Tree/solution.java
+++ 100-Same-Tree/solution.java
@@ -9,12 +9,9 @@
  */
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q==null) return true;
-        else if (p == null && q != null) return false;
-        else if (p!= null && q == null) return false;
-        else{
-            if (p.val!=q.val) return false;
-            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
-        }
+        if (p==null && q == null) return true;
+        else if (p==null || q==null) return false;
+        else if (p.val!=q.val) return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
