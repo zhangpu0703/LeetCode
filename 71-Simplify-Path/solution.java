c2f6316a9744c4cc3bfa06131b74aef5b08e9d41
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-08-18 11:40:17 -0400

    [71. Simplify Path][Accepted]committed by LeetCode Extension

--- 71-Simplify-Path/solution.java
+++ 71-Simplify-Path/solution.java
@@ -1,20 +1,23 @@
 public class Solution {
     public String simplifyPath(String path) {
-        List<String> list = new ArrayList<String>();
         String[] strs = path.split("/");
-        for (String str : strs){
-            if (str.equals("..") && list.size() > 0) list.remove(list.size()-1);
-            else if (str.equals("..") || str.equals(".")|| str.equals("")) continue;
-            else list.add(str);
+        Stack<String> stack = new Stack<>();
+        for (int i=0; i<strs.length; i++){
+            String str = strs[i];
+            if (str.equals(".")||str.length()==0) continue;
+            else if (str.equals("..")){
+                if (!stack.isEmpty()) stack.pop();
+            }
+            else stack.push(str);
         }
+        List<String> list = new ArrayList<>();
+        while (!stack.isEmpty()) list.add(stack.pop());
         
-        StringBuilder res = new StringBuilder();
         if (list.size()==0) return "/";
-        for (int i = 0; i<list.size(); i++){
-            res.append('/');
-            res.append(list.get(i));
+        StringBuilder sb = new StringBuilder();
+        for (int i=list.size()-1; i>=0; i--){
+            sb.append("/").append(list.get(i));
         }
-        return res.toString();
-        
+        return sb.toString();
     }
 }
