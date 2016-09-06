316bfb1d54cb8ab53cc063a19122bc071811a717
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-06 16:34:06 -0400

    [23. Merge k Sorted Lists][Time Limit Exceeded]committed by LeetCode Extension

--- 23-Merge-k-Sorted-Lists/solution.java
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -7,27 +7,25 @@
  * }
  */
 public class Solution {
+    private Comparator<ListNode> nodeComp = new Comparator<ListNode>(){
+        public int compare(ListNode a, ListNode b){
+            return a.val-b.val;
+        }
+    };
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode dummy = new ListNode(0);
-        if (lists.length == 0) return dummy.next;
-        ListNode temp = dummy;
-        Comparator<ListNode> nodeComp = new Comparator<ListNode>(){
-            @Override
-            public int compare(ListNode l1, ListNode l2){
-                return l1.val-l2.val;
-            }
-        };
-        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(lists.length,nodeComp);
-        for (int i = 0; i<lists.length; i++){
-            if (lists[i]!=null) queue.offer(lists[i]);
+        int k = lists.length;
+        if (k==0) return null;
+        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(k,nodeComp);
+        for (ListNode node : lists) {
+            if (node != null) pq.offer(node);
         }
-        while (!queue.isEmpty()){
-            ListNode cur = queue.poll();
-            temp.next = cur;
-            if (cur.next != null) queue.offer(cur.next);
-            temp = temp.next;
-            
+        ListNode res = new ListNode(0), cur = res;
+        while (!pq.isEmpty()){
+            ListNode node = pq.poll();
+            cur.next = new ListNode(node.val);
+            if (node.next != null) pq.offer(node.next);
+            cur = cur.next;
         }
-        return dummy.next;
+        return res.next;
     }
 }
