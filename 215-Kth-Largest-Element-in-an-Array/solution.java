3596017c8d6d6fff5c0a8f9eca11a9bd865437fd
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-11 21:54:26 -0400

    [215. Kth Largest Element in an Array][Time Limit Exceeded]committed by LeetCode Extension

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -3,27 +3,28 @@ public class Solution {
         k = nums.length-k;
         int start = 0, end = nums.length-1;
         while (start<end){
-            int ind = partition(nums,start,end);
-            if (ind == k) return nums[ind];
-            else if (ind<k) start = ind+1;
-            else end = ind-1;
+            int pivot = quickselect(nums,start,end);
+            if (pivot==k) return nums[pivot];
+            else if (pivot<k) start = pivot+1;
+            else end = pivot;
         }
         return nums[start];
     }
-    public int partition(int[] nums, int start, int end){
+    public int quickselect(int[] nums, int start, int end){
+        if (start>=end) return start;
         int pivot = end;
         int i = start, j = end;
-        while (i<j){
-            while (i<j && nums[i]<=nums[pivot]) i++;
+        while(i<j){
+            while(i<j && nums[i]<=nums[pivot]) i++;
             while (i<j && nums[j]>=nums[pivot]) j--;
             if (i<j) swap(nums,i,j);
         }
         swap(nums,j,pivot);
         return i;
     }
-    public void swap(int[] nums, int a, int b){
-        int temp = nums[a];
-        nums[a]=nums[b];
-        nums[b]=temp;
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
