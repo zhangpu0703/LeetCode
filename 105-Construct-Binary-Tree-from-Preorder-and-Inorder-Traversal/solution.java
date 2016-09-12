a96bdc5bd4260c862a1c6e6abaa58dadeed4ab1b
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-12 15:56:26 -0400

    [105. Construct Binary Tree from Preorder and Inorder Traversal][Time Limit Exceeded]committed by LeetCode Extension

--- 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/solution.java
+++ 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/solution.java
@@ -9,15 +9,18 @@
  */
 public class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder.length == 0) return null;
-        int rootVal = preorder[0];
-        TreeNode root = new TreeNode(rootVal);
-        int i=0;
-        while (inorder[i]!=rootVal) i++;
-        int[] leftIn = Arrays.copyOfRange(inorder,0,i);
-        int[] rightIn = Arrays.copyOfRange(inorder,i+1,inorder.length);
-        int[] leftPre = Arrays.copyOfRange(preorder,1,i+1);
-        int[] rightPre = Arrays.copyOfRange(preorder,i+1,preorder.length);
+        int n = preorder.length;
+        if (n==0) return null;
+        int val = preorder[0];
+        TreeNode root = new TreeNode(val);
+        int len = 0;
+        while(len<n){
+            if (inorder[len]==val) break;
+        }
+        int[] leftPre = Arrays.copyOfRange(preorder,1,len+1);
+        int[] rightPre = Arrays.copyOfRange(preorder,len+1,n);
+        int[] leftIn = Arrays.copyOfRange(inorder,0,len);
+        int[] rightIn = Arrays.copyOfRange(inorder,len+1,n);
         root.left = buildTree(leftPre,leftIn);
         root.right = buildTree(rightPre,rightIn);
         return root;
