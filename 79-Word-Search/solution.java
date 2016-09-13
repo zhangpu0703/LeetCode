da087009d22a20b7fdb9f45043325b657010c689
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-12 21:11:06 -0400

    [79. Word Search][Accepted]committed by LeetCode Extension

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -1,29 +1,28 @@
 public class Solution {
     public boolean exist(char[][] board, String word) {
         int m = board.length;
+        if (m==0) return false;
         int n = board[0].length;
-        boolean[][] used = new boolean[m][n];
+        boolean[][] visited = new boolean[m][n];
         for (int i=0; i<m; i++){
             for (int j=0; j<n; j++){
-                if (search(board,used,word,i,j,0)) return true;
+                if (helper(board,word,visited,0,i,j)) return true;
             }
         }
         return false;
     }
-    public boolean search(char[][] board, boolean[][] used, String word, int i, int j, int cur){
-        
-        if (cur == word.length()) return true;
-        if (i<0 || i>=board.length || j<0 || j>=board[0].length) return false;
-        if (word.charAt(cur)!=board[i][j]) return false;
-        if (!used[i][j]){
-            used[i][j]=true;
-            if (search(board,used,word,i-1,j,cur+1)) return true;
-            if (search(board,used,word,i+1,j,cur+1)) return true;
-            if (search(board,used,word,i,j-1,cur+1)) return true;
-            if (search(board,used,word,i,j+1,cur+1)) return true;
-            used[i][j]=false;
-            return false;
-        }
-        else return false;
+    public boolean helper(char[][] board, String word, boolean[][] visited, int ind, int i, int j){
+        if (ind == word.length()) return true;
+        int m = board.length, n = board[0].length;
+        if (i<0 || i>=m || j<0 || j>=n) return false;
+        if (visited[i][j]) return false;
+        if (board[i][j]!=word.charAt(ind)) return false;
+        visited[i][j]=true;
+        if(helper(board,word,visited,ind+1,i-1,j)) return true; 
+        if(helper(board,word,visited,ind+1,i,j-1)) return true; 
+        if(helper(board,word,visited,ind+1,i+1,j)) return true; 
+        if(helper(board,word,visited,ind+1,i,j+1)) return true;
+        visited[i][j]=false;
+        return false;
     }
 }
