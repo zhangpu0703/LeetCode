0f8d19e2a9f2e38101b2dc435239f236b14fa3ab
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-12 21:45:03 -0400

    [392. Is Subsequence][Accepted]committed by LeetCode Extension

--- 392-Is-Subsequence/solution.java
+++ 392-Is-Subsequence/solution.java
@@ -1,14 +1,13 @@
 public class Solution {
     public boolean isSubsequence(String s, String t) {
-        int m = s.length(), n = t.length();
-        boolean[][] dp = new boolean[m+1][n+1];
-        for (int i=0; i<=n; i++) dp[0][i]=true;
-        for (int i=1; i<=m; i++){
-            for (int j=1; j<=n; j++){
-                dp[i][j]=dp[i][j-1];
-                if (dp[i-1][j-1] && s.charAt(i-1) == t.charAt(j-1)) dp[i][j]=true;
+        int i = 0, j = 0;
+        while (i<s.length()){
+            if (j<t.length()){
+                if (t.charAt(j) == s.charAt(i)) i++;
+                j++;
             }
+            else break;
         }
-        return dp[m][n];
+        return i==s.length();
     }
 }
