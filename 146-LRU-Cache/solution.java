719f27de7c4502f392fbcae0bbf89411a5414b71
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-17 23:03:42 -0400

    [146. LRU Cache][Accepted]committed by LeetCode Extension

--- 146-LRU-Cache/solution.java
+++ 146-LRU-Cache/solution.java
@@ -1,32 +1,72 @@
+class DNode{
+    DNode prev;
+    DNode next;
+    int value;
+    int key;
+}
+
 public class LRUCache {
-    private HashMap<Integer,Integer> map;
-    private List<Integer> list;
-    private int kk;
+    private void remove(DNode cur){
+        DNode prev = cur.prev, next = cur.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+    private void add(DNode cur){
+        cur.next = tail;
+        cur.prev = tail.prev;
+        tail.prev.next = cur;
+        tail.prev = cur;
+    }
+    private void movetotail (DNode cur){
+        remove(cur);
+        add(cur);
+    }
+    private int size;
+    private int capacity;
+    private HashMap<Integer,DNode> map;
+    private DNode head, tail;
+    
     public LRUCache(int capacity) {
-        map = new HashMap<Integer, Integer>();
-        list = new LinkedList<Integer>();
-        kk = capacity;
+        this.capacity = capacity;
+        head = new DNode();
+        tail = new DNode();
+        head.next = tail;
+        tail.prev = head;
+        map = new HashMap<Integer,DNode>();
+        size = 0;
     }
     
     public int get(int key) {
-        if (map.containsKey(key)) {
-            list.remove(list.indexOf(key));
-            list.add(key);
-            return map.get(key);
+        if (!map.containsKey(key)) {
+            
+            return -1;
         }
-        else return -1;
+        DNode node = map.get(key);
+        movetotail(node);
+        return node.value;
+        
     }
     
     public void set(int key, int value) {
         if (map.containsKey(key)){
-            list.remove(list.indexOf(key));
+            DNode cur = map.get(key);
+            cur.value = value;
+            map.put(key,cur);
+            movetotail(cur);
         }
-        list.add(key);
-        map.put(key,value); 
-        if (list.size()>kk){
-            map.remove(list.get(0));
-            list.remove(0);
+        else{
+            DNode node = new DNode();
+            node.key = key;
+            node.value = value;
+            map.put(key,node);
+            add(node);
+            size++;
+            if (size>capacity){
+                DNode removed = head.next;
+                remove(removed);
+                map.remove(removed.key);
+                size--;
+            }
         }
-        
     }
 }
