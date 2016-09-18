ae3815b469fc24f035e5beb5f86dc03417aebc37
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-17 20:57:14 -0400

    [20. Valid Parentheses][Accepted]committed by LeetCode Extension

--- 20-Valid-Parentheses/solution.java
+++ 20-Valid-Parentheses/solution.java
@@ -1,22 +1,21 @@
 public class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        char[] c = s.toCharArray();
-        for (int i=0; i<c.length; i++){
-            if (stack.isEmpty()){
-                if (c[i]==')' || c[i]==']'|| c[i]=='}') return false;
-            }
-            
-            if (c[i]=='(' || c[i]=='['|| c[i]=='{') stack.push(c[i]);
-            
-            if (c[i]==')'){
-                if (stack.pop() != '(') return false;
-            }
-            if (c[i]==']'){
-                if (stack.pop() != '[') return false;
-            }
-            if (c[i]=='}'){
-                if (stack.pop() != '{') return false;
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i<s.length(); i++){
+            char cur = s.charAt(i);
+            if (cur == '(' || cur =='[' || cur == '{') stack.push(cur);
+            else{
+                if (stack.isEmpty()) return false;
+                char prev = stack.pop();
+                if (cur == ')'){
+                    if (prev!='(') return false;
+                }
+                else if (cur == ']'){
+                    if (prev!='[') return false;
+                }
+                else{
+                    if (prev!='{') return false;
+                }
             }
         }
         return stack.isEmpty();
