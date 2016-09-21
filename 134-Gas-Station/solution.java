acddbc6ab7be7829ef45ead7deb2892e69aed6fb
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-20 23:16:45 -0400

    [134. Gas Station][Accepted]committed by LeetCode Extension

--- 134-Gas-Station/solution.java
+++ 134-Gas-Station/solution.java
@@ -1,17 +1,17 @@
 public class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int gasTotal = 0;
-        int costTotal = 0;
-        for (int g : gas) gasTotal+=g;
-        for (int c : cost) costTotal +=c;
+        int gasTotal = 0, costTotal = 0;
+        for (int i=0; i<gas.length; i++) {
+            gasTotal += gas[i];
+            costTotal += cost[i];
+        }
         if (gasTotal<costTotal) return -1;
-        int cur = 0;
-        int res = 0;
+        int res = 0, cur = 0;
         for (int i=0; i<gas.length; i++){
-            cur += gas[i]-cost[i];
+            cur = cur+gas[i]-cost[i];
             if (cur<0){
-                res=i+1;
                 cur = 0;
+                res = i+1;
             }
         }
         return res;
