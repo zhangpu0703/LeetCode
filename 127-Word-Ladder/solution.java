bdf3d7210b7a13664f026971d4326fee2a190809
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-20 22:09:36 -0400

    [127. Word Ladder][Time Limit Exceeded]committed by LeetCode Extension

--- 127-Word-Ladder/solution.java
+++ 127-Word-Ladder/solution.java
@@ -1,41 +1,43 @@
 public class Solution {
-    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
-        wordList.add(endWord);
-        Queue<String> queue = new LinkedList<String>();
+     public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
         HashSet<String> visited = new HashSet<String>();
+        wordList.add(endWord);
+        int res = 1;
+        Queue<String> queue = new LinkedList<String>();
         queue.offer(beginWord);
         visited.add(beginWord);
-        int res = 1;
         while (!queue.isEmpty()){
-            int size = queue.size();
             res++;
+            int size = queue.size();
             for (int i=0; i<size; i++){
                 String cur = queue.poll();
-                List<String> ladders = getLadders(cur,wordList);
-                for (String ladder : ladders) {
-                    if (ladder.equals(endWord)) return res;
-                    if (visited.contains(ladder)) continue;
-                    visited.add(ladder);
-                    queue.offer(ladder);
+                System.out.println(cur);
+                List<String> list = ladders(cur,wordList);
+                for (String s : list) {
+                    System.out.println(s);
+                    if (s.equals(endWord)) return res;
+                    if (visited.contains(s)) continue;
+                    visited.add(s);
+                    queue.offer(s);
                 }
             }
-            
         }
         return 0;
     }
-    public List<String> getLadders (String word, Set<String> wordList){
-        char[] c = word.toCharArray();
-        List<String> res = new ArrayList<String>();
-        for (int i=0; i<c.length; i++){
-            char cur = c[i];
-            for (char temp = 'a'; temp<='z'; temp++){
-                if (temp==cur) continue;
-                c[i]=temp;
-                String ladder = String.valueOf(c);
-                if (wordList.contains(ladder)) res.add(ladder); 
+    public List<String> ladders(String word, Set<String> wordList){
+        char[] arr = word.toCharArray();
+        List<String> ladders = new ArrayList<String>();
+        for (int i=0; i<arr.length; i++){
+            char cur = arr[i];
+            for (char c = 'a'; c<='z'; c++){
+                if (c!=cur){
+                    arr[i]=c;
+                    String ladder = String.valueOf(arr);
+                    if (wordList.contains(ladder)) ladders.add(ladder);
+                }
             }
-            c[i]=cur;
+            arr[i]=cur;
         }
-        return res;
+        return ladders;
     }
 }
