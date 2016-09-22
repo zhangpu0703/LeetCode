971ad6248c37ffa72bc56d383f8d6b692865f265
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-22 11:21:34 -0400

    [67. Add Binary][Time Limit Exceeded]committed by LeetCode Extension

--- 67-Add-Binary/solution.java
+++ 67-Add-Binary/solution.java
@@ -1,36 +1,19 @@
 public class Solution {
     public String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
-        int lenA = a.length()-1;
-        int lenB = b.length()-1;
-        int flag = 0;
-        while (lenA>=0 || lenB>=0){
-            int curA=0;
-            int curB=0;
+        int indA = a.length()-1, indB = b.length()-1;
+        int carry = 0;
+        while(indA >= 0 || indB >= 0){
             int sum = 0;
-            if (lenA>=0) curA=a.charAt(lenA)-'0';
-            if (lenB>=0) curB=b.charAt(lenB)-'0';
-            sum=flag+curA+curB;
-            if (sum==3){
-                sb.append(1);
-                flag=1;
-            }
-            else if (sum==2){
-                sb.append(0);
-                flag=1;
-            }
-            else if (sum==1){
-                sb.append(1);
-                flag=0;
-            }
-            else{
-                sb.append(0);
-                flag=0;
-            }
-            lenA--;
-            lenB--;
+            if (indA<0) sum = b.charAt(indB)-'0'+carry;
+            else if (indB<0) sum = a.charAt(indA)-'0'+carry;
+            else sum = a.charAt(indA)-'0'+b.charAt(indB)-'0'+carry;
+            sb.append(sum%2);
+            carry = sum/2;
+            indA--;
+            indB--;
         }
-        if (flag==1) sb.append(1);
-        return sb.reverse().toString();
+        if (carry==1) sb.append(1);
+        return sb.toString();
     }
 }
