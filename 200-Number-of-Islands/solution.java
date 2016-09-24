afbef6da866a9896c24e04836c4658e7905cb54f
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-24 19:09:43 -0400

    [200. Number of Islands][Accepted]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -1,23 +1,25 @@
 public class Solution {
     public int numIslands(char[][] grid) {
         int res = 0;
+        if (grid == null || grid.length == 0) return res;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
         for (int i=0; i<grid.length; i++){
             for (int j=0; j<grid[i].length; j++){
-                if (grid[i][j] == '1'){
+                if (grid[i][j] == '1' && !visited[i][j]){
                     res++;
-                    helper(grid,i,j);
+                    helper(grid,i,j,visited);
                 }
             }
         }
         return res;
     }
-    public void helper(char[][] grid, int i, int j){
+    public void helper(char[][] grid, int i, int j, boolean[][] visited){
         if (i<0 || i>=grid.length || j<0 || j>=grid[i].length) return;
-        if (grid[i][j]=='0') return;
-        grid[i][j]= '0';
-        helper(grid,i-1,j);
-        helper(grid,i+1,j);
-        helper(grid,i,j-1);
-        helper(grid,i,j+1);
+        if (grid[i][j]=='0' || visited[i][j]) return;
+        visited[i][j] = true;
+        helper(grid,i-1,j,visited);
+        helper(grid,i+1,j,visited);
+        helper(grid,i,j-1,visited);
+        helper(grid,i,j+1,visited);
     }
 }
