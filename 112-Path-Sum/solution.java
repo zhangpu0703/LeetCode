6ea27f0d149dbe370fb2b4aaf9ff9aaa3e3b9cc4
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-24 16:32:20 -0400

    [112. Path Sum][Accepted]committed by LeetCode Extension

--- 112-Path-Sum/solution.java
+++ 112-Path-Sum/solution.java
@@ -10,7 +10,17 @@
 public class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) return false;
-        if (root.left == null && root.right == null && root.val == sum) return true;
+        //else if (root == null) return true;
+        if (root.left == null && root.right == null){
+            if (root.val == sum) return true;
+            else return false;
+        }
+        if (root.left == null){
+            return hasPathSum(root.right,sum-root.val);
+        }
+        if (root.right == null){
+            return hasPathSum(root.left,sum-root.val);
+        }
         return hasPathSum(root.left,sum-root.val) || hasPathSum(root.right,sum-root.val);
     }
 }
