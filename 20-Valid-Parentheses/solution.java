d65ca1953f6daf42f2ad4e297052e18fff69c078
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-24 21:17:53 -0400

    [20. Valid Parentheses][Accepted]committed by LeetCode Extension

--- 20-Valid-Parentheses/solution.java
+++ 20-Valid-Parentheses/solution.java
@@ -1,21 +1,17 @@
 public class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<Character>();
-        for (int i = 0; i<s.length(); i++){
-            char cur = s.charAt(i);
-            if (cur == '(' || cur =='[' || cur == '{') stack.push(cur);
-            else{
-                if (stack.isEmpty()) return false;
-                char prev = stack.pop();
-                if (cur == ')'){
-                    if (prev!='(') return false;
-                }
-                else if (cur == ']'){
-                    if (prev!='[') return false;
-                }
-                else{
-                    if (prev!='{') return false;
-                }
+        for(int i=0; i<s.length(); i++){
+            Character cur = s.charAt(i);
+            if (cur == '(' || cur == '[' || cur == '{') stack.push(cur);
+            if (cur == ')'){
+                if (stack.isEmpty() || stack.pop() != '(') return false;
+            }
+            if (cur == ']'){
+                if (stack.isEmpty() || stack.pop() != '[') return false;
+            }
+            if (cur == '}'){
+                if (stack.isEmpty() || stack.pop() != '{') return false;
             }
         }
         return stack.isEmpty();
