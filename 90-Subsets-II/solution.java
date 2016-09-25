05694a945ebcef8c66965b57c5af83467769bbc9
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-24 21:34:06 -0400

    [90. Subsets II][Accepted]committed by LeetCode Extension

--- 90-Subsets-II/solution.java
+++ 90-Subsets-II/solution.java
@@ -3,16 +3,17 @@ public class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         boolean[] visited = new boolean[nums.length];
-        helper(new ArrayList<Integer>(), 0, nums, visited);
+        helper(nums,0,new ArrayList<Integer>(), visited);
         return res;
     }
-    public void helper(List<Integer> path, int start, int[] nums, boolean[] visited){
+    public void helper(int[] nums, int ind, List<Integer> path, boolean[] visited){
         res.add(new ArrayList<Integer>(path));
-        for (int i=start; i<nums.length; i++){
-            if (i>0 && !visited[i-1] && nums[i]==nums[i-1]) continue;
-            path.add(nums[i]);
+        if (ind == nums.length) return;
+        for (int i = ind; i<nums.length; i++){
+            if (i>0 && !visited[i-1] && nums[i-1] == nums[i]) continue;
             visited[i]=true;
-            helper(path, i+1, nums, visited);
+            path.add(nums[i]);
+            helper(nums, i+1, path, visited);
             visited[i]=false;
             path.remove(path.size()-1);
         }
