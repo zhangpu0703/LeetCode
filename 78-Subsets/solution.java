3cf2df560cbe78ce900612bc2cb25c0407cb3e5e
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-24 21:27:22 -0400

    [78. Subsets][Accepted]committed by LeetCode Extension

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -1,16 +1,15 @@
 public class Solution {
+    private List<List<Integer>> res = new ArrayList<List<Integer>>();
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        //Arrays.sort(nums);
-        helper(res, new ArrayList<Integer>(), 0, nums);
+        helper(nums,0,new ArrayList<Integer>());
         return res;
     }
-    
-    public void helper(List<List<Integer>> res, List<Integer> path, int start, int[] nums){
+    public void helper(int[] nums, int ind, List<Integer> path){
         res.add(new ArrayList<Integer>(path));
-        for (int i = start; i<nums.length; i++){
+        if (ind == nums.length) return;
+        for (int i=ind; i<nums.length; i++){
             path.add(nums[i]);
-            helper(res, path, i+1, nums);
+            helper(nums,i+1, path);
             path.remove(path.size()-1);
         }
     }
