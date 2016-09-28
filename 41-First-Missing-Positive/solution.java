4109406e1f007ea1396242b180cbc8711217d0fe
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-28 13:24:36 -0400

    [41. First Missing Positive][Time Limit Exceeded]committed by LeetCode Extension

--- 41-First-Missing-Positive/solution.java
+++ 41-First-Missing-Positive/solution.java
@@ -1,16 +1,19 @@
 public class Solution {
     public int firstMissingPositive(int[] nums) {
-        for (int i=0; i<nums.length; i++){
-            while (nums[i]>0 && nums[i]<=nums.length && nums[i]!=nums[nums[i]-1]){
-                //if () break;
-                int temp = nums[i];
-                nums[i] = nums[nums[i]-1];
-                nums[temp-1]=temp;
+        if (nums == null || nums.length == 0) return 1;
+        for (int i = 0; i<nums.length; i++){
+            while (nums[i]<=nums.length && nums[i]>0 && nums[nums[i]-1] != i){
+                swap(nums,i,nums[i]-1);
             }
         }
         for (int i=0; i<nums.length; i++){
             if (nums[i]!=i+1) return i+1;
         }
         return nums.length+1;
+    }
+    public void swap (int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j]=temp;
     }
 }
