2e3ea86d6792fc29e23d22fda1e4eff5f4d0c041
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-09-28 13:25:00 -0400

    [41. First Missing Positive][Accepted]committed by LeetCode Extension

--- 41-First-Missing-Positive/solution.java
+++ 41-First-Missing-Positive/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public int firstMissingPositive(int[] nums) {
         if (nums == null || nums.length == 0) return 1;
         for (int i = 0; i<nums.length; i++){
-            while (nums[i]<=nums.length && nums[i]>0 && nums[nums[i]-1] != i){
+            while (nums[i]<=nums.length && nums[i]>0 && nums[nums[i]-1] != nums[i]){
                 swap(nums,i,nums[i]-1);
             }
         }
