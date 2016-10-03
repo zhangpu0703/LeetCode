88e7b6ef40726b4b31cf11f150cd484648443152
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-10-02 20:23:29 -0400

    [17. Letter Combinations of a Phone Number][Accepted]committed by LeetCode Extension

--- 17-Letter-Combinations-of-a-Phone-Number/solution.java
+++ 17-Letter-Combinations-of-a-Phone-Number/solution.java
@@ -3,14 +3,16 @@ public class Solution {
         String[] map = new String[]{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         List<String> res = new ArrayList<String>();
         for (int i=0; i<digits.length(); i++){
-            if (res.size()==0)  res.add("");
-            String next = map[digits.charAt(i)-'0'];
-            for (int j=0; j<res.size(); j++){
-                for (int k=0; k<next.length(); k++){
-                    System.out.println(res.get(j));
-                    res.add(res.get(j)+next.charAt(k));
+            char[] cur = map[digits.charAt(i)-'0'].toCharArray();
+            if (res.size()==0) res.add("");
+            List<String> list = new ArrayList<String>();
+            for (int j=0; j<cur.length; j++){
+                
+                for (String s : res){
+                    list.add(s+cur[j]);
                 }
             }
+            res = new ArrayList<String>(list);
         }
         return res;
     }
