1e50a951a40c185d3d39e31288f429f26a15f761
zhangpu0703 <zhang_pu0703@hotmail.com>
2016-10-03 22:21:35 -0400

    [324. Wiggle Sort II][Accepted]committed by LeetCode Extension

--- 324-Wiggle-Sort-II/solution.java
+++ 324-Wiggle-Sort-II/solution.java
@@ -2,14 +2,13 @@ public class Solution {
     public void wiggleSort(int[] nums) {
         int[] res = Arrays.copyOf(nums,nums.length);
         Arrays.sort(res);
-        int mid;
-        if (res.length%2==0) mid = res.length/2-1;
-        else mid=res.length/2;
-        int curInd = 0;
+        int mid = res.length/2;
+        if (res.length%2==0) mid--;
+        int ind = 0;
         for (int i=0; i<=mid; i++){
-            nums[curInd] = res[mid-i];
-            if (curInd+1<res.length) nums[curInd+1] = res[nums.length-i-1];
-            curInd+=2;
+            nums[ind]=res[mid-i];
+            if (ind+1<nums.length)  nums[ind+1] = res[res.length-1-i];
+            ind+=2;
         }
     }
 }
